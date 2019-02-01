c276b42239b5902b8a81b9a9d5124d9acaea15c8
jgo31 <jgo31@gatech.edu>
2019-02-01 00:20:36 -0500

    Added recompute_compnent, Require to check the heesian input

--- block_new.py
+++ block_new.py
@@ -1,29 +1,45 @@
 from fenics import *
 from fenics_adjoint import *
+
 from pyadjoint import Block
 
+from dot_to_function import dot_to_function
+
+backend_dot_to_function = dot_to_function
+
 class UpdatedBlock(Block):
-	def __init__(self, func, mat, **kwargs):
+	def __init__(self, func, mat, array, **kwargs):
 		super(UpdatedBlock, self).__init__()
 		self.kwargs = kwargs
 		self.umat = mat
+		self.ar = array
+		self.y = self.umat.T
 		# self.add_dependency(func.block_variable)
 
 	def __str__(self):
 		return "UpdatedBlock"
 
+	def recompute_component(self, inputs, block_variable, idx, prepared):
+		return backend_dot_to_function(inputs[0],inputs[1],inputs[2])
+
 	def evaluate_adj_component(self, inputs, adj_inputs, block_variable, idx, prepared=None):
 		adj_input = adj_inputs[0]
 		# self.umat.shape[1]
-		y = self.umat.T
-		return y.inner(adj_input)
+		#y = self.umat.T
+		return self.y.inner(adj_input)
 
 	def evaluate_hessian_component(self, inputs, hessian_inputs, adj_inputs, block_variable, idx,
                                    relevant_dependencies, prepared=None):
-		adj_input = adj_inputs[0]
-		y = self.umat.T
-		y1 = y.inner(adj_input)
-		return  y.T.dot(y1)
+		hessian_input_0 = hessian_inputs[0]
+		hessian_input_1 = heesian_inputs[1]
+		#y = self.umat.T
+		y1 = self.y.inner(hessian_input_0)
+		y2 = self.y.T.dot(y1)
+		## Todo
+		## Two input?
+		y3 = y2.dot(hessian_input_1)
+		
+		return  self.y.T.dot(y1)
 		
 		"""This method must be overridden.
 
